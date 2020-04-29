6e561b7e021a1df25503fb22a2b1bc7559f95b69
longlee218 <longlehoang2013@gmail.com>
2020-04-29 16:02:13 +0700

    kaka

---
+++ DCSVN/src/main/java/com/example/demo/entity/BaseEntity.java
@@ -0,0 +1,61 @@
+package com.example.demo.entity;
+
+import org.springframework.data.annotation.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Column
+    @CreatedBy
+    private String createdBy;
+
+    @Column
+    @CreatedDate
+    private Date createdDate;
+
+    @Column
+    @LastModifiedBy
+    private String modifiedBy;
+
+    @Column
+    @LastModifiedDate
+    private Date modifiedDate;
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getModifiedBy() {
+        return modifiedBy;
+    }
+
+    public void setModifiedBy(String modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
---
+++ DCSVN/src/main/java/com/example/demo/entity/NewsEntity.java
@@ -0,0 +1,65 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "new")
+public class NewsEntity  extends  BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "thumb_nail")
+    private  String thumbNail;
+
+    @Column(name = "short_description")
+    private String shortDescription;
+
+    @Column(name = "content")
+    private String content;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getThumbNail() {
+        return thumbNail;
+    }
+
+    public void setThumbNail(String thumbNail) {
+        this.thumbNail = thumbNail;
+    }
+
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
